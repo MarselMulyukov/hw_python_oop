f1f154e16f31c4f55d0a9211a7d2192ccd46f9b4
Марсель Мулюков <MarekMulin@yandex.ru>
2021-02-16 01:55:32 +0500

    Changes after first review

--- homework.py
+++ homework.py
@@ -6,37 +6,38 @@ class Calculator:
         self.limit = limit
         self.records = []
 
+    def today_remained(self):
+        return self.limit - self.get_today_stats()
+
     def add_record(self, record):
         self.records.append(record)
 
     def get_today_stats(self):
+        '''Здесь тоже использовал List comprehensions'''
         today = dt.datetime.now().date()
-        today_spent = 0
-        for record in self.records:
-            if record.date == today:
-                today_spent += record.amount
-        return today_spent
+        list_of_spendings = [record.amount for record in self.records if
+                             record.date == today]
+        return sum(list_of_spendings)
 
     def get_week_stats(self):
+        '''Берем даты большие или равные первому дню последних семи дней'''
         today = dt.datetime.now().date()  # дата сегодня
-        week_ago = today - dt.timedelta(7)  # дата того дня, с которого
-        week_spent = 0                    # берется отсчет последних семи дней
-        for record in self.records:
-            if record.date >= week_ago and record.date <= today:
-                week_spent += record.amount
-        return week_spent
+        week_ago = today - dt.timedelta(6)  # первый из последних семи дней
+        list_of_spendings = [record.amount for record in self.records if
+                             week_ago <= record.date <= today]
+        return sum(list_of_spendings)
 
 
 class Record:
-    def __init__(self, amount, comment, date=dt.datetime.now().date()):
+    def __init__(self, amount, comment, date=None):
         self.amount = amount
         self.comment = comment
-        if date != dt.datetime.now().date():  # если необязательный параметр
+        if date:  # если необязательный параметр
             date_format = '%d.%m.%Y'  # date передан в аргументах экземпляра
             moment = dt.datetime.strptime(date, date_format)  # приводим дату
             self.date = moment.date()  # к формату '%Y.%m.%d'
         else:
-            self.date = date
+            self.date = dt.datetime.now().date()
 
 
 class CashCalculator(Calculator):
@@ -44,30 +45,27 @@ class CashCalculator(Calculator):
     EURO_RATE = 89.33
 
     def get_today_cash_remained(self, currency: str):
-        balance = self.limit - self.get_today_stats()  # текущий остаток денег
-        if currency == 'rub':                          # на сегодня
-            abs_balance = abs(balance)  # баланс по модулю
-            currency_txt = 'руб'
-        elif currency == "eur":
-            abs_balance = round(abs(balance) / self.EURO_RATE, 2)  # баланс по
-            currency_txt = 'Euro'  # модулю в евро, два знака после запятой
-        else:
-            abs_balance = round(abs(balance) / self.USD_RATE, 2)  # то же
-            currency_txt = 'USD'                             # самое в доллары
+        '''Здесь использовал тоже return без else. Не доволен количеством
+        задействуемых переменных. Может быть что-то подскажешь?'''
+        currencies_dict = {'rub': (1, 'руб'), 'eur': (self.EURO_RATE, 'Euro'),
+                           'usd': (self.USD_RATE, 'USD')}
+        balance = self.today_remained()
+        abs_balance = abs(balance)
+        recounted = round(abs_balance / currencies_dict[currency][0], 2)
+        currency_txt = currencies_dict[currency][1]
         if balance > 0:
-            return f'На сегодня осталось {abs_balance} {currency_txt}'
+            return (f'На сегодня осталось {recounted}'
+                    f' {currency_txt}')
         elif balance == 0:
             return 'Денег нет, держись'
-        else:
-            return (f'Денег нет, держись: твой долг - {abs_balance}'
-                    + f' {currency_txt}')
+        return (f'Денег нет, держись: твой долг - {recounted}'
+                f' {currency_txt}')
 
 
 class CaloriesCalculator(Calculator):
     def get_calories_remained(self):
-        balance = self.limit - self.get_today_stats()
+        balance = self.today_remained()
         if balance > 0:
             return ('Сегодня можно съесть что-нибудь ещё, но с общей'
-                    + f' калорийностью не более {balance} кКал')
-        else:
-            return 'Хватит есть!'
+                    f' калорийностью не более {balance} кКал')
+        return 'Хватит есть!'
